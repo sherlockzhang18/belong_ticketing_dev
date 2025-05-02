ab058c76d55b2d9cc912a1aa06e82a413c87f53c
Sherlock Zhang <jiaxi.zhang@wustl.edu>
2025-05-02 03:06:29 -0500

    django basics

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app01 import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    
+    # www.xxx.com/index/ -> call function
+    path('index/', views.index),
 ]
